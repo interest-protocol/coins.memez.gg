0d68170d198d342a5c3d37be8b40afcf0d6de0d8
git-marcopitra <marcomorete73@hotmail.com>
2025-01-11 02:20:46 +0100

    :iphone: fix: add viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html lang="en">
     <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+      />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 
