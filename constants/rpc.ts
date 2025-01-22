fa3323f7f393de5132d57d84f68bd60ce7137e8d
git-marcopitra <marcomorete73@hotmail.com>
2025-01-22 05:45:49 +0100

    :sparkles: feat: add new rpc logic

--- constants/rpc.ts
+++ constants/rpc.ts
@@ -1,11 +1,13 @@
-export const RPC_STORAGE_KEY = 'coins-memez-rpc';
+import { Network } from './network';
+
+export const RPC_STORAGE_KEY = 'coins-memez-rpc-key';
 
 export enum RPC {
-  Shinami = 'https://api.shinami.com/node/v1/sui_testnet_8d0574f74958d67931463c51da36bc24',
-  Mysten = 'https://fullnode.testnet.sui.io:443',
-  Blockvision = 'https://sui-testnet-endpoint.blockvision.org',
-  SuiScan = 'https://rpc-testnet.suiscan.xyz/',
-  Suiet = 'https://testnet.suiet.app',
+  Shinami = 'shinami',
+  Mysten = 'mysten',
+  Blockvision = 'blockvision',
+  SuiScan = 'suiscan',
+  Suiet = 'suiet',
 }
 
 export const RPCs = [
@@ -22,4 +24,23 @@ export const RPC_DISPLAY = {
   [RPC.Blockvision]: 'Blockvision',
   [RPC.SuiScan]: 'SuiScan',
   [RPC.Suiet]: 'Suiet',
+};
+
+export const RPC_MAP: Record<Network, Record<RPC, string>> = {
+  [Network.TESTNET]: {
+    [RPC.Shinami]:
+      'https://api.shinami.com/node/v1/sui_testnet_8d0574f74958d67931463c51da36bc24',
+    [RPC.Mysten]: 'https://fullnode.testnet.sui.io:443',
+    [RPC.Blockvision]: 'https://sui-testnet-endpoint.blockvision.org',
+    [RPC.SuiScan]: 'https://rpc-testnet.suiscan.xyz',
+    [RPC.Suiet]: 'https://testnet.suiet.app',
+  },
+  [Network.MAINNET]: {
+    [RPC.Shinami]:
+      'https://api.shinami.com/node/v1/sui_mainnet_f8ba2ad72d9ad60899e56d2f9d813e2b',
+    [RPC.Mysten]: 'https://fullnode.mainnet.sui.io:443',
+    [RPC.Blockvision]: 'https://sui-mainnet-endpoint.blockvision.org',
+    [RPC.SuiScan]: 'https://rpc-mainnet.suiscan.xyz',
+    [RPC.Suiet]: 'https://mainnet.suiet.app',
+  },
 };
