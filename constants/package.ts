573a702cc41266a5ae72110da823e812b72fcc83
Marco <marcomorete73@hotmail.com>
2025-02-25 03:34:08 +0100

    :sparkles: feat: add revert changes

--- constants/package.ts
+++ constants/package.ts
@@ -4,5 +4,5 @@ export const IPX_COIN_STANDARD = {
   [Network.TESTNET]:
     '0x03979c0d3fac9c7cd89bc3b0b8de0aff4f23369be980b789b06c1b104f90eff3',
   [Network.MAINNET]:
-    '0x7ead93e49fe002193faec3d2e4a7b750e9e568b5d875cafe17fcb0dc672b075e',
+    '0x8135dda2a2b575ef87654aa1115a46da63ea9a387abb5f7cdb93df062af7a937',
 };
